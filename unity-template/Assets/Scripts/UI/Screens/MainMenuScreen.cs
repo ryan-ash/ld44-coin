2efa78853fb8ce22635cdf95d87650bcaef21231
Ryan Ash <scarletspark@gmail.com>
2019-04-28 21:01:29 +0300

    improved coin splash, runner script partial

--- unity-template/Assets/Scripts/UI/Screens/MainMenuScreen.cs
+++ unity-template/Assets/Scripts/UI/Screens/MainMenuScreen.cs
@@ -8,11 +8,17 @@ public class MainMenuScreen : ScreenBaseController {
     public RectTransform icon;
     public CanvasGroup text;
 
+    public Sprite frontSprite, backSprite;
+
+    private Image iconImage;
+    private float currentXScale;
+
     private bool introAnimationOver = false;
     private bool animationReversed = false;
 
     void Start() {
         instance = this;
+        iconImage = icon.GetComponent<Image>();
     }
 
     public void OnEnable() {
@@ -30,6 +36,11 @@ public class MainMenuScreen : ScreenBaseController {
             (float value) => {
                 var xScaleAlpha = (animationReversed) ? 1f - value : value;
                 var xScale = Mathf.Lerp(1f, -1f, xScaleAlpha);
+                bool flipCondition = (xScale < 0 && currentXScale >= 0 && !animationReversed) || (xScale > 0 && currentXScale <= 0 && animationReversed);
+                if (flipCondition) {
+                    iconImage.sprite = (animationReversed) ? frontSprite : backSprite;
+                }
+                currentXScale = xScale;
                 var textOpacity = SettingsManager.instance.introAnimationMinAlpha + Mathf.Abs(value - 0.5f);
                 icon.localScale = new Vector3(xScale, 1f, 1f);
                 text.alpha = textOpacity;
